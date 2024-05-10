38a1e4195091bed066dc45d031c5e640fc5bfd84
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-05-10 09:50:44 -0300

    initial commit

---
+++ Controllers/ProcesosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiApp.Controllers
+{
+    public class ProcesosController : Controller
+    {
+        // GET: Procesos
+        public string Listado()
+        {
+            return "Listado de procesos";
+        }
+    }
+}
