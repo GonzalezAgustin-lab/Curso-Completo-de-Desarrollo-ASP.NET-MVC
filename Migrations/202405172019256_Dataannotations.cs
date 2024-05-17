d36f5498744d09072c83b2c8887c14280ca0c3ec
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-05-17 17:44:18 -0300

    Validación de datos

---
+++ Migrations/202405172019256_Dataannotations.cs
@@ -0,0 +1,18 @@
+﻿namespace MiApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Dataannotations : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Clientes", "Nombre", c => c.String(maxLength: 60));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Clientes", "Nombre", c => c.String());
+        }
+    }
+}
--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MiApp.Models
 {
     public class Clientes
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Nombre { get; set; }
+
+        [Display (Name = "Fecha de Alta")]
+        [DataType (DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+
         public DateTime FechaAlta { get; set; }
+
+        [Range (18,75)]
         public int Edad {  get; set; }
     }
 
