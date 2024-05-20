e4a2ae81d221cf3ec48a4e0f7ad0387c2e4858a9
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-05-20 11:46:51 -0300

    Autenticación y autorización P2

---
+++ Autenticacion/Controllers/AccesoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Autenticacion.Controllers
+{
+    [Authorize(Users = "prueba@hola.com")]
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ContentResult Privado()
+        {
+            return Content("Privado");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Publico()
+        {
+            return Content("Publico");
+        }
+    }
+}
