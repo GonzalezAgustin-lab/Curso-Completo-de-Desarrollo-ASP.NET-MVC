65f0a9eb4f07ee80dcf3d5348b9d873d70a0a6fe
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-05-21 11:52:09 -0300

    Scaffolding

---
+++ MiScaffoldingMVC/Models/Clientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MiScaffoldingMVC.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int edad { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        //DbSet<Clientes> Clientes { get; set; }
+
+        public System.Data.Entity.DbSet<MiScaffoldingMVC.Models.Clientes> Clientes { get; set; }
+    }
+}
