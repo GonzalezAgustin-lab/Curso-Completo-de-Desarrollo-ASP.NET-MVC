5fffe12c763d4b73868a7b3fc3a1b54799088517
Gonzalez Agustin <agusgonzalez603@hotmail.com>
2024-05-22 11:29:48 -0300

    Jquery

---
+++ MiListaMVC/Controllers/SampleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiListaMVC.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            List<string> lista = new List<string>();
+            lista.Add("Seleccionar");
+            lista.Add("Argentina");
+            lista.Add("Bolivia");
+            lista.Add("Colombia");
+
+            SelectList Paises = new SelectList(lista);
+            ViewData["Paises"] = Paises;
+            return View();
+        }
+
+        public JsonResult Provincias(string Pais) 
+        {
+            List<string> ListProv = new List<string>();
+            switch(Pais)
+            {
+                case "Argentina":
+                    ListProv.Add("Buenos Aires");
+                    ListProv.Add("Entre Rios");
+                    break;
+                case "Colombia":
+                    ListProv.Add("Medellin");
+                    ListProv.Add("Bogota");
+                    break;
+                case "Bolivia":
+                    ListProv.Add("La Paz");
+                    ListProv.Add("Santa Cruz");
+                    break;
+            }
+            return Json(ListProv);
+        }
+    }
+}
